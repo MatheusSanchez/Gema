53b42c89dc42741b7837900fdf355e7c0f046349
Matheus Sanchez <matheus2.sanchez@usp.br>
2018-05-01 22:08:18 -0300

    Problema da soma do vetor accepted, olhar porque funcionou kkkkkk

---
+++ cf/heads_or_tails.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+	int n;
+	float p_c;
+
+	cin << n << p_c;
+
+	
+
+
+
+}
--- cf/pega_nao_pega.cpp
+++ cf/pega_nao_pega.cpp
@@ -3,28 +3,34 @@
 using namespace std;
 
 
-int bt(int soma_atual, int soma_dese, vector<int> v, int cout, int pos){
-	/*if(soma_atual > soma_dese){
-		return 0;
-	}if(soma_atual == soma_dese){
-		return 1;
-	}else{
-		int aux = v.size();
-		for (; pos < aux; pos++){
-			soma_atual += v[pos];
-			if(bt(soma_atual,soma_dese,v,cout,pos+1) == 1){
-				soma_atual -= v[pos];
-				cout++;
-			}else{
-				break;
-			}
+
+int c = 0;
+
+void bt(int soma_atual, int soma_dese, vector<int> v, int ind){
+
+	//cout << "S " << soma_atual << endl;
+
+	if(soma_atual == soma_dese){
+		c++;
+		//cout << "Retorno c++" << endl;	
+	
+	}
+		//cout << "For ind " << ind << endl;
+		for (int i = ind; i < v.size(); i++){	
+				//if(soma_atual <= soma_dese){
+					//cout << "Mandando " << soma_atual << " i " << i << endl;
+			bt(soma_atual+v[i],soma_dese,v,i+1);	
+					//cout << "return " << soma_atual << " i " << i << endl;
+				//}
 		}
-	}*/
-
-
+	
+	
+		
 }
 
 int main (){
+
+	ios :: sync_with_stdio(false);
 
 	int k,soma;
 	vector<int> v;
@@ -40,9 +46,24 @@ int main (){
 	}
 
 
-	for (int i = 0; i < v.size(); ++i){
+	
+
+	
+	//sort(v.begin(),v.end());
+	
+	/*for (int i = 0; i < v.size(); ++i){
 		cout << v[i] << " ";
 	}
+	cout << endl;*/
+
+	//bt(v[0],soma,v,0+1);
+
+	for (int i = 0; i <= v.size(); ++i){
+			
+		bt(v[i],soma,v,i+1);
+	}
+	cout << c;
+
 
 
 
