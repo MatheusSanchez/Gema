7a16fde8ecafabf29a1d0bdc3df5e300213cd044
Matheus Sanchez <matheus2.sanchez@usp.br>
2018-08-02 21:06:50 -0300

    Sem sucesso :(

--- uva/p12442.cpp
+++ uva/p12442.cpp
@@ -19,7 +19,9 @@ int main(int argc, char const *argv[]){
 
 		vi g(n_marc);
 		vi visit(n_marc,-1);
-		int maior = -1;
+		vi dist(n_marc,0);
+		
+		int maior = 0;
 
 		for (int k = 0; k < n_marc; ++k){
 
@@ -29,34 +31,58 @@ int main(int argc, char const *argv[]){
 			g[A] = B;
 
 		}
-		int flow = 0;
 
 		for (int k = 0; k < n_marc; k++){
 
+		//	cout << "k " << k << endl;
 			int count = 1;
 			int u = k;
-			visit[u] = 1;
-			u = g[u];
 
 			while(visit[u] != 1){
-			
+				//cout << "u " << u << endl;
 				visit[u] = 1;
 				u = g[u];
 				count++;
-			}
+			}		
+			//cout << "sai " << k << endl;
+			dist[k] = count + dist[u];	
+
 			
-			if(count > maior){
+			/*cout << endl;
+			cout<< "dist " << endl;
+			for (int z = 0; z < n_marc; ++z){
+				cout << dist[z] << " ";
+			}
+			cout << endl;
+			cout<< "visitados " << endl;
+		
+			for (int z = 0; z < n_marc; ++z){
+				cout << visit[z] << " ";
+			}
+			cout << endl;	*/
+
+
+			if(dist[k] > maior){
 				resp[i] = k+1;
-				maior = count;
+				maior = dist[k];
+			}
+			if(u == k && count > 1){
+				u = g[u];
+				int ultimo = u;
+				while(u != k){
+					dist[u] = dist[k];
+					ultimo = u;
+					u = g[u];
+				}
+				k = ultimo;
 			}	
-			fill(visit.begin(),visit.end(),-1);
+			//fill(visit.begin(),visit.end(),-1);
 		}
 
 
 	}
 	for (int i = 0; i < cases; ++i){
 		cout << "Case "<< i+1 << ": " << resp[i] << endl;
-
 	}
 
 	
