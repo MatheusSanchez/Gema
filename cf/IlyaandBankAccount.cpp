a66601ad86e4bcb0e57001466f81f4dc7105fd63
Matheus Sanchez <matheus2.sanchez@usp.br>
2018-08-28 18:31:51 -0300

    Joaozão problems

--- cf/IlyaandBankAccount.cpp
+++ cf/IlyaandBankAccount.cpp
@@ -1,4 +1,3 @@
-// template fera
 #include <bits/stdc++.h>
 
 #define true 1
@@ -19,6 +18,28 @@ typedef vector<v_p_i> graph; // vector de vector pair int; (lista de adjacencia)
 int main (){
 
 	ios :: sync_with_stdio(false);
+
+	int n,n1,n2,aux;
+
+	cin >> n;
+
+	if(n>0){
+		cout << n << endl;
+		return 0;
+	}
+
+	aux = n%10; 
+
+	n /= 10;
+	n1 = n;
+
+	n /= 10;
+	n2 = n*10 + aux;
+
+	//cout << n1 << " " << n2 << endl;
+
+	cout << max(n1,n2) << endl;
+
 
 
 }
